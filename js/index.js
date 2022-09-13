5818ff6069435f497986a49b0be57a60f803e1ad
jawnhoang <johnhoang5@gmail.com>
2022-09-12 23:08:46 -0700

    eyeCU project page added and done. 3 more projs to do

--- js/index.js
+++ js/index.js
@@ -34,3 +34,48 @@ function dirname(path){
 }
 
 // REDIRECTS FOR NAV BAR END //
+
+
+// REDIRECTS FOR PROJECT CARDS BEGIN //
+
+function eyeCU(){
+  var dirPath = dirname(location.href);
+  fullPath = dirPath + "/eyeCU.html";
+  window.location = fullPath;
+}
+
+
+// REDIRECTS FOR PROJECT CARD END
+
+
+
+// SLIDE SHOW CONTROLS BEGIN //
+var slideIndex = 1;
+  showSlides(slideIndex);
+  
+  // Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+  
+  // Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+  
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+// SLIDE SHOW CONTROLS END //
